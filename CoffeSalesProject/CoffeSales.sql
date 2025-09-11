c58e8a55348f962d393ae4b0eb615b9cfb435234
unknown <farhanaaktersuci@gmail.com>
2025-09-12 03:33:59 +0600

    Schema Added for Coffe Sales

---
+++ CoffeSalesProject/CoffeSales.sql
@@ -0,0 +1,52 @@
+--Create table city 
+DROP TABLE 
+IF 
+EXISTS
+city;
+CREATE TABLE city
+(
+	city_id	INT PRIMARY KEY,
+	city_name VARCHAR(15),	
+	population	BIGINT,
+	estimated_rent	FLOAT,
+	city_rank INT
+);
+
+--Create table customers 
+DROP TABLE 
+IF EXISTS
+customers;
+
+CREATE TABLE customers
+(
+	customer_id INT PRIMARY KEY,	
+	customer_name VARCHAR(25),	
+	city_id INT,
+	CONSTRAINT fk_city FOREIGN KEY (city_id) REFERENCES city(city_id)
+);
+
+DROP TABLE products;
+
+CREATE TABLE products
+(
+	product_id	INT PRIMARY KEY,
+	product_name VARCHAR(35),	
+	Price float
+);
+--Create table sales
+DROP TABLE  IF EXISTS sales;
+
+CREATE TABLE sales
+(
+	sale_id	INT PRIMARY KEY,
+	sale_date	date,
+	product_id	INT,
+	customer_id	INT,
+	total FLOAT,
+	rating INT,
+	CONSTRAINT fk_products FOREIGN KEY (product_id) REFERENCES products(product_id),
+	CONSTRAINT fk_customers FOREIGN KEY (customer_id) REFERENCES customers(customer_id) 
+);
+
+--End of Schema
+
